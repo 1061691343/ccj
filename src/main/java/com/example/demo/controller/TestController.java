9168257c396e66e7e84381e7962ecc0963b47217
ccj <1061691343@qq.com>
2020-08-05 08:59:10 +0800

    第三次修改

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @RequestMapping("/hello")
     public  String abc(){
-        return "aabfcd";
+        return "aabffcd";
     }
 }
