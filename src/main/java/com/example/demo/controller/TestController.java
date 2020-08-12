b512d4c296657ca1cf387f37c7f976b7eb9bb755
ccj <1061691343@qq.com>
2020-08-12 13:02:18 +0800

    第17次修改

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @RequestMapping("/hello")
     public  String abc(){
-        return "鼎业机械123";
+        return "鼎业的机械123";
     }
 }
